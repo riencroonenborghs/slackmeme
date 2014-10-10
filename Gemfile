a1a5036cef1b9cca9ff94cde93f4d320a391f14a
Rien Croonenborghs <rien@croonenborghs.net>
2014-10-10 16:13:54 +1300

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.1.6'
+gem 'sqlite3'
+gem 'dotenv-rails'
+gem 'unirest'
+
+# gem 'sass-rails', '~> 4.0.3'
+# gem 'uglifier', '>= 1.3.0'
+# gem 'coffee-rails', '~> 4.0.0'
+# gem 'coffee-haml'
+
+# gem 'jquery-rails'
+# gem 'turbolinks'
+# gem 'jbuilder', '~> 2.0'
+
---
+++ app/service/meme_generator.rb
@@ -0,0 +1,49 @@
+class MemeGenerator
+  IMGFLIP_URL = "https://api.imgflip.com/caption_image"
+
+  class << self
+    def generate(message)
+      meme = match_memes(message)
+      return {error: 'meme not found'} unless meme
+
+      response = Unirest.post 
+        IMGFLIP_URL,
+        parameters: {
+          username:     ENV['IMGFLIP_USERNAME'],
+          password:     ENV['IMGFLIP_PASSWORD'],
+          template_id:  meme[:id], 
+          text0:        meme[:top], 
+          text1:        meme[:bottom]
+        }
+       
+      image_url = response.body['data']['url']
+      { image_url: image_url }
+    end
+
+  private
+    
+    def match_memes(message)
+      case message.downcase
+      when /^(one does not simply)(.*)/
+        return { id: 61579, top: $1, bottom: $2.strip! }
+      when /^(what if i told you)(.*)/
+        return { id: 100947, top: $1, bottom: $2.strip! }
+      when /^(brace yourselves)(.*)/
+        return { id: 61546, top: $1, bottom: $2.strip! }
+      when /^(.*)(but that(?:')?s none of my business)/
+        return { id: 16464531, top: $1.strip!, bottom: $2 }
+      when /^(.*)((all the)(.*))/
+        return { id: 61533, top: $1.strip!, bottom: $2 }
+      when /^(.*)(ain(?:')?t nobody got time for that)/
+        return { id: 442575, top: $1.strip!, bottom: $2 }
+      when /^(.*)(we(?:')?re dealing with a badass over here)/
+        return { id: 11074754, top: $1.strip!, bottom: $2 }
+      when /^(.*?)((a)+nd it(?:')?s gone)\z/
+        return { id: 766986, top: $1.strip!, bottom: $2 }
+      else
+        return nil
+      end
+    end
+
+  end
+end
